ab2c8dc5260d46732c1876740e1c9435d1e11bb4
Slijkhuis <slijkhuis.thijs@gmail.com>
2025-12-07 20:17:58 +0100

    day 7 part 2

--- 07/main.go
+++ 07/main.go
@@ -58,5 +58,34 @@ func part1() {
 }
 
 func part2() {
-	fmt.Println("TODO")
+	g := aoc.BuildGridFromFile(os.Args[2])
+	startPoint := g.MustFindFirstPointWithValue('S')
+	timelines := map[aoc.Point]int{}
+	timelines[startPoint] = 1
+	for y := 0; y < g.Height; y++ {
+		for x := 0; x < g.Width; x++ {
+			p := aoc.Point{X: x, Y: y}
+			v, ok := g.Data[p]
+			if !ok {
+				continue
+			}
+			switch v {
+			case 'S', '.':
+				pDown := p.Move(aoc.Down)
+				timelines[pDown] += timelines[p]
+			case '^':
+				pLeft := p.Move(aoc.Left).Move(aoc.Down)
+				pRight := p.Move(aoc.Right).Move(aoc.Down)
+				timelines[pLeft] += timelines[p]
+				timelines[pRight] += timelines[p]
+			}
+		}
+	}
+
+	numberOfTimelines := 0
+	for x := 0; x < g.Width; x++ {
+		numberOfTimelines += timelines[aoc.Point{X: x, Y: g.Height - 1}]
+	}
+
+	fmt.Println(numberOfTimelines)
 }
